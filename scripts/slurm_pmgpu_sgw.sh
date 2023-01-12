b28aed4a7eb58703abddf8c3dfb4e9a6bac03548
Phillip Thomas <pthomas@lbl.gov>
2023-01-12 15:45:04 -0800

    more scripts added

--- scripts/slurm_pmgpu_sgw.sh
+++ scripts/slurm_pmgpu_sgw.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #
 # Slurm job script for running StochasticGW on Perlmutter-GPU
-# Set '-n <#-MPI-proceses>' below; uses up to 4 MPI orocesses, 4 GPUs per node
+# Set '--ntasks=<#-MPI-proceses>' below; limit 4 MPI orocesses, 4 GPUs per node
 #
-#SBATCH -A m1759
-#SBATCH -q debug
-#SBATCH -n 1
-#SBATCH -t 0:29:59
 
-#SBATCH -C gpu
-#SBATCH -c 32
+#SBATCH --account=<your-account>
+#SBATCH --constraint=gpu
+#SBATCH --qos=debug
+
+#SBATCH --time=00:29:59
+#SBATCH --ntasks=4
+
 #SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=32
 #SBATCH --gpus-per-task=1
 #SBATCH --gpu-bind=map_gpu:0,1,2,3
+
 #SBATCH -J sGW
 #SBATCH -o %x-%j.out
 #SBATCH -e %x-%j.err
@@ -23,3 +26,4 @@ module load cray-fftw
 export SLURM_CPU_BIND="cores"
 
 srun sgw.x
+
